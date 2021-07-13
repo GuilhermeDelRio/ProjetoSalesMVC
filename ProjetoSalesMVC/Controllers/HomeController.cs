65adae2829a3981abbd10470918dcc7423c32855
Guilherme Del Rio <guilherme@gplpm.com.br>
2021-07-13 19:33:46 -0300

    Primeiro Model-Controller-View - Derpartment

--- ProjetoSalesMVC/Controllers/HomeController.cs
+++ ProjetoSalesMVC/Controllers/HomeController.cs
@@ -5,6 +5,7 @@ using System.Collections.Generic;
 using System.Diagnostics;
 using System.Linq;
 using System.Threading.Tasks;
+using ProjetoSalesMVC.Models.ViewModels;
 
 namespace ProjetoSalesMVC.Controllers {
     public class HomeController : Controller {
--- ProjetoSalesMVC/Models/ViewModels/ErrorViewModel.cs
+++ ProjetoSalesMVC/Models/ViewModels/ErrorViewModel.cs
@@ -1,6 +1,6 @@
 using System;
 
-namespace ProjetoSalesMVC.Models {
+namespace ProjetoSalesMVC.Models.ViewModels {
     public class ErrorViewModel {
         public string RequestId { get; set; }
 
