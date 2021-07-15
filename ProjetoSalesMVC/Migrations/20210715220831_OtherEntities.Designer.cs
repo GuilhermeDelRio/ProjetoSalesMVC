b3bd415cbb5e80857a4faee63aa0311defe7b567
Guilherme Del Rio <guilherme@gplpm.com.br>
2021-07-15 19:11:28 -0300

    Outras entidades

--- ProjetoSalesMVC/Migrations/20210715220831_OtherEntities.Designer.cs
+++ ProjetoSalesMVC/Migrations/20210715220831_OtherEntities.Designer.cs
@@ -2,15 +2,17 @@
 using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using ProjetoSalesMVC.Data;
 
 namespace ProjetoSalesMVC.Migrations
 {
     [DbContext(typeof(ProjetoSalesMVCContext))]
-    partial class ProjetoSalesMVCContextModelSnapshot : ModelSnapshot
+    [Migration("20210715220831_OtherEntities")]
+    partial class OtherEntities
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
